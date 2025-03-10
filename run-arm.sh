58575ff0966a2f57254314a65c52cf9861e9d04e
asopaipa <eslounicoquese@gmail.com>
2025-03-10 16:06:45 +0100

    Create run-arm.sh

---
+++ run-arm.sh
@@ -0,0 +1,9 @@
+if [[ $ALLOW_REMOTE_ACCESS == "yes" ]];then
+    EXTRA_FLAGS="$EXTRA_FLAGS --bind-all --allow-remote=1 --allow-remote-access=1"
+fi
+
+COMMAND_ARGS="--client-console --live-cache-type memory --live-mem-cache-size 104857600 --disable-sentry --log-stdout --client-console --http-port ${HTTP_PORT} ${EXTRA_FLAGS}"
+
+
+cd /acestream
+exec /acestream/python/bin/python ./main.py $COMMAND_ARGS
