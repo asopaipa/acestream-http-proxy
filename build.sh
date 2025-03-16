db60b6ce0b8eabd5b708ba9c9dd80948ce45094a
asopaipa <eslounicoquese@gmail.com>
2025-03-16 20:37:12 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
-
 IMAGE_NAME="algomlop/acestream-http-proxy"
 IMAGE_TAG="latest"
 
+ARCH=$(uname -m)
+
+case "$ARCH" in
+    "x86_64") 
+        DOCKERFILE="Dockerfile.amd64"
+        PLATFORM="linux/amd64"
+        ;;
+    "aarch64") 
+        DOCKERFILE="Dockerfile.arm64"
+        PLATFORM="linux/arm64"
+        ;;
+    "armv7l") 
+        DOCKERFILE="Dockerfile.armv7"
+        PLATFORM="linux/arm/v7"
+        ;;
+    *) 
+        echo "Not supported architecture: $ARCH"
+        exit 1
+        ;;
+esac
+
+
 if ! docker buildx inspect mybuilder &>/dev/null; then
-  echo "Creating new builder multi-platform..."
+  echo "Creating builder multi-plataforma..."
   docker buildx create --name mybuilder --use
 fi
 
@@ -13,9 +34,10 @@ docker buildx use mybuilder
 
 echo "Building..."
 docker buildx build \
-  --platform linux/amd64,linux/arm64,linux/arm/v7 \
+  --platform $PLATFORM \
+  -f $DOCKERFILE \
   -t "${IMAGE_NAME}:${IMAGE_TAG}" \
   --push \
   .
 
-echo "Completed"
+echo "Completed."
